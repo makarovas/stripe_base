eb41dc0477d971b17ae259f7fd9784806022a5ea
Alexander Makarof <makarofalex@gmail.com>
2020-07-08 23:07:13 +0300

    started web

--- app/src/index.js
+++ app/src/index.js
@@ -4,6 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { Elements} from '@stripe/react-stripe-js'
+import {loadStripe} from '@stripe/stripe-js';
+
+
+export const stripePromise = loadStripe(
+  'pk_test_51H2jRCK2oXBcLUP6kQaTIt1qlPEnYUqCmrppMV8Hb7r3nUDcTl6tQhLH699IWlxZxC9iepZmkCQ9LTSo80IvT0AZ00UEvyeaD9'
+).then
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -11,7 +19,5 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
 serviceWorker.unregister();
