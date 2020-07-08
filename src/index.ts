0f3f3c874ebc28df3b55fb7c4866c3e8dcdef7fa
Alexander Makarof <makarofalex@gmail.com>
2020-07-08 19:39:03 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import {config} from 'dotenv';
+if(process.env.NODE_ENV !== 'dev') {
+  config();
+}
+
+
+import Stripe from 'stripe';
+export const stripe = new Stripe(process.env.STRIPE_SECRET, {
+  apiVersion: '2020-03-02'
+})
+
+import {app} from './api';
+
+const port = process.env.PORT || 3333;
+app.listen(port);
